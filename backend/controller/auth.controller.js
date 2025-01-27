0093c954e1a7c0695c0cc4206b8fee625981b538
khaalid <khaalid958@gmail.com>
2025-01-27 16:55:55 +0300

    adding basic authrouters

---
+++ backend/controller/auth.controller.js
@@ -0,0 +1,10 @@
+
+export const signup = (req,res)=>{
+    res.send("sign up router 🥇🏆")
+}
+export const login = (req,res)=>{
+    res.send("login  router 🥇🏆")
+}
+export const logout = (req,res)=>{
+    res.send("logout  router 🥇🏆")
+}
